6583c3113321c17e10bf5b2c1d030ef78e327777
smcfadden150 <smm576@drexel.edu>
2019-03-04 00:00:49 -0500

    MainMenuSettingsComplete
    Main menu scene added
    Shader for bluring elements behind the tooltips
    Includes setting menu that will save changes to a settings xml file
    Will display popup menu if the settings fail to change

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public CheckersSettings settings;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        Debug.Log(Application.dataPath);
+        //on start load the settings file
+        settings = CheckersSettings.DeserializeSettingsFile(Application.dataPath + "/");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public bool SaveChanges()
+    {
+        try
+        {
+            CheckersSettings.SerializeSettingsFile(Application.dataPath + "/", settings);
+            return true;
+        }
+        catch(Exception ex)
+        {
+            return false;
+        }
+    }
+    public void LoadDefaultSettings()
+    {
+        if(this.settings != null)
+        {
+            this.settings.LoadDefaults();
+        }
+    }
+
+    public void SetPlayerColorIndex(int i)
+    {
+        this.settings.PlayerColorIndex = i;
+    }
+    public void SetCompetitorColorIndex(int i)
+    {
+        this.settings.PlayerColorIndex = i;
+    }
+    public void SetCombinedMessageSize(string s)
+    {
+        try
+        {
+            int value = int.Parse(s);
+
+            this.settings.MaxCombinedReliableMessageSize = value;
+        }
+        catch(Exception ex)
+        {
+            this.settings.MaxCombinedReliableMessageSize = 1024;
+        }
+    }
+    public void SetMessageSize(string s)
+    {
+        try
+        {
+            int value = int.Parse(s);
+
+            this.settings.MaxMessageQueueSize = value;
+        }
+        catch (Exception ex)
+        {
+            this.settings.MaxMessageQueueSize = 1024;
+        }
+    }
+    public void SetNetworkPort(string s)
+    {
+        this.settings.DefaultNetworkPort = s;
+    }
+    public void SetNetworkAddress(string s)
+    {
+        this.settings.DefaultNetworkAddress = s;
+    }
+    public void SetRoomName(string s)
+    {
+        this.settings.RoomName = s;
+    }
+    public void SetPlayerName(string s)
+    {
+        this.settings.PlayerName = s;
+    }
+}
---
+++ Assets/Scripts/Settings/CheckersSettings.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+using System;
+using UnityEngine;
+using System.Diagnostics;
+
+[System.Serializable]
+public class CheckersSettings
+{
+    public string PlayerName;
+    public string RoomName;
+    public string DefaultNetworkAddress;
+    public int PlayerColorIndex;
+    public int CompetitorColorIndex;
+    public string DefaultNetworkPort;
+    public string DefaultServerError;
+    public int MaxMessageQueueSize;
+    public int MaxCombinedReliableMessageSize;
+    public float ThreadAwakeTimeout;
+
+    public CheckersSettings()
+    {
+        PlayerName = "Default Name";
+        RoomName = "Room Name";
+        DefaultNetworkAddress = "0.0.0.00";
+        PlayerColorIndex = 0;
+        CompetitorColorIndex = 0;
+        DefaultNetworkPort = "0000";
+        MaxMessageQueueSize = 1024;
+        MaxCombinedReliableMessageSize = 2048;
+        ThreadAwakeTimeout = 10.0f;
+    }
+    public void LoadDefaults()
+    {
+        PlayerName = "Default Name";
+        RoomName = "Room Name";
+        DefaultNetworkAddress = "0.0.0.00";
+        PlayerColorIndex = 0;
+        CompetitorColorIndex = 0;
+        DefaultNetworkPort = "0000";
+        MaxMessageQueueSize = 1024;
+        MaxCombinedReliableMessageSize = 2048;
+        ThreadAwakeTimeout = 10.0f;
+    }
+    public static CheckersSettings DeserializeSettingsFile(string path)
+    {
+        var fileName = path + "settings.xml";
+        UnityEngine.Debug.Log(fileName);
+        CheckersSettings tmp = new CheckersSettings();
+        try
+        {
+            if (File.Exists(fileName))
+            {
+                using (var r = new StreamReader(fileName, false))
+                {
+                    XmlSerializer deserializer = new XmlSerializer(typeof(CheckersSettings));
+                    tmp = (CheckersSettings)deserializer.Deserialize(r);
+                    return tmp;
+                }
+            }
+            else
+            {
+                //if the file doesnt exist, create a new object, save it, then reload it
+                CheckersSettings newObj = new CheckersSettings();
+                CheckersSettings.SerializeSettingsFile(path, newObj);
+                CheckersSettings.DeserializeSettingsFile(path);
+            }
+            return tmp;
+        }
+        catch(Exception e)
+        {
+            Trace.WriteLine(e);
+            return tmp;
+        }
+    }
+
+    public static void SerializeSettingsFile(string path, CheckersSettings settings)
+    {
+        var fileName = path + "settings.xml";
+        UnityEngine.Debug.Log(fileName);
+        //load settings.xml or create a new file
+        try
+        {
+            using (var s = new StreamWriter(fileName, false))
+            {
+                XmlSerializer serailizer = new XmlSerializer(typeof(CheckersSettings));
+                serailizer.Serialize(s, settings);
+            }
+        }
+        catch(Exception e)
+        {
+            Trace.WriteLine(e);
+        }
+    }
+}
