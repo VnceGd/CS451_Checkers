1806dcd32d2aa62690407f39b89a41aaacab2bc3
Vincent Nguyen <vtn28@drexel.edu>
2019-03-11 16:34:27 -0400

    UI Additions
    - Add content to About and Credits screens
    - Add "Leave Match" button to Game scene
    - Centered board on screen, flipped perspective for client

---
+++ Assets/Scripts/LeaveMatch.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LeaveMatch : MonoBehaviour
+{
+    public void TraverseToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
